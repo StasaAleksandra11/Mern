0673e810da1942431bad5e332ad54f49dae327d1
Aleksandra Comagic <aleksandra@Aleksandras-MacBook-Air.local>
2025-03-03 13:56:21 +0100

    zavrsno

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,13 @@ import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/store';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+
+if (import.meta.env.MODE === 'production') {
+    disableReactDevTools();
+  }
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <StrictMode>
         <BrowserRouter>
